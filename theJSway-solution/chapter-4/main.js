8c312543423bdb5d7876483711344d61cca8f227
kadir <idia_2008@hotmail.com>
2023-04-04 19:01:04 +0300

    add new file

--- theJSway-solution/chapter-4/main.js
+++ theJSway-solution/chapter-4/main.js
@@ -77,3 +77,29 @@ for (let i=1; i <= 10; i++) {
 /*5. Neither yes nor no
 Write a program that plays "neither yes, nor no" with the user. Specifically, the programs asks 
 the user to enter text until either "yes" or "no" is typed, which ends the game.*/
+
+let userWord = "";
+while (!(userWord === "no" || userWord === "yes")) {
+  userWord = prompt('Enter a word!');
+}
+
+/*6. FizzBuzz
+Write a program that shows all numbers between 1 and 100 with the following exceptions:
+It shows "Fizz" instead if the number is divisible by 3.
+It shows "Buzz" instead if the number is divisible by 5 and not by 3.
+When it's done, improve it so that the program shows "FizzBuzz" instead for numbers divisible 
+both by 3 and by 5.*/
+
+for (let i = 1; i <= 100; i++) {
+  if((i % 3 === 0) && (i % 5 === 0)) {
+    console.log(`${i} is fizzbuzz`);
+  }
+  else if (i % 3 === 0) {
+    console.log(`${i} is fizz`);
+  }
+  else if (i % 5 === 0) {
+    console.log(`${i} is buzz`);
+  }else {
+    console.log(`${i}`)
+  }
+}
