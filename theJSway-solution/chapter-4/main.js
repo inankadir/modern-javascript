26a8e03ea65e1ce7a3b642f8ec5d970fd1cbbe8f
kadir <idia_2008@hotmail.com>
2023-04-06 02:16:17 +0300

    add new file

--- theJSway-solution/chapter-4/main.js
+++ theJSway-solution/chapter-4/main.js
@@ -103,3 +103,48 @@ for (let i = 1; i <= 100; i++) {
     console.log(`${i}`)
   }
 }
+
+// Looping a triangle: Write a loop that makes seven calls to console.log to output the following triangle:
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+let tag = "#";
+while (tag.length <= 7) {
+  console.log(tag);
+  tag += "#";
+}
+
+// Chessboard : Write a program that creates a string that represents an 8*8 grid, using newline 
+// characters to separate lines. At each position of the grid there is either a space or a "#" characters
+// The characters should form a chessboard.
+// Passing this string to console.log should show something like this:
+//  # # # #
+// # # # # 
+//  # # # #
+//  # # # #
+// # # # # 
+//  # # # #
+//  # # # #
+// # # # # 
+// When you have a program that generates this pattern, define a binding size = 8 and change the program 
+// so that it works for any size, outputting a grid of the given width and height.
+
+let size = 8
+let board = '';
+for (let row = 1; row <= size; row++) {
+  for (let column = 1; column <= size; column++) {
+    if ((row+column) % 2 == 0) {
+      board += ' '
+    }else {
+      board += '#'
+    }
+  }
+  board += '/n'
+}
+console.log(board)
+
