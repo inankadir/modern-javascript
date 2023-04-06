1273f4e3109ac138a82b188b9bc415cc96c7315a
kadir <idia_2008@hotmail.com>
2023-04-06 04:29:54 +0300

    add new file

--- theJSway-solution/chapter-4/main.js
+++ theJSway-solution/chapter-4/main.js
@@ -148,3 +148,53 @@ for (let row = 1; row <= size; row++) {
 }
 console.log(board)
 
+//Minimum : The previous chapter introduced the standard function Math.min that returns its smallest 
+//argument. We can build something like that now. Write a function min that takes two arguments and 
+//returns their minimum.
+
+// Your code here.
+function min(num1, num2) {
+  return (num1 < num2) ? num1 : num2
+}
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
+
+//Recursion: We’ve seen that % (the remainder operator) can be used to test whether a number is 
+//even or odd by using % 2 to see whether it’s divisible by two. Here’s another way to define whether 
+//a positive whole number is even or odd:
+
+//Zero is even.
+
+//One is odd.
+
+//For any other number N, its evenness is the same as N - 2.
+
+//Define a recursive function isEven corresponding to this description. The function should 
+//accept a single parameter (a positive, whole number) and return a Boolean.
+
+//Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
+
+// Your code here.
+function isEven(num) {
+  if(num >= 0) {
+    if (num === 0) {
+      return true
+    }else if (num === 1) {
+      return false
+    }else {
+      return(isEven(num-2))
+    }
+  }else {
+    return "negative"
+  }
+} 
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
+
