279fa226632b0b136e5225d67489b06fa30221ce
kadir <idia_2008@hotmail.com>
2023-04-06 07:02:24 +0300

    add new file

--- theJSway-solution/chapter-4/main.js
+++ theJSway-solution/chapter-4/main.js
@@ -198,3 +198,38 @@ console.log(isEven(75));
 console.log(isEven(-1));
 // → ??
 
+//Bean counting : You can get the Nth character, or letter, from a string by writing "string"[N].
+//The returned value will be a string containing only one character (for example, "b"). The first 
+//character has position 0, which causes the last one to be found at position string.length - 1. 
+//In other words, a two-character string has length 2, and its characters have positions 0 and 1.
+
+//Write a function countBs that takes a string as its only argument and returns a number that indicates 
+//how many uppercase “B” characters there are in the string.
+
+//Next, write a function called countChar that behaves like countBs, except it takes a second argument 
+//that indicates the character that is to be counted (rather than counting only uppercase “B” characters).
+//Rewrite countBs to make use of this new function.
+
+// Your code here.
+function countBs(str) {
+  let count = 0;
+  for(let i = 0; i <= str.length-1; i++) {
+    if(str[i] === "B") {
+      count += 1
+    }
+  }
+  return count;
+}
+console.log(countBs("BBC"));
+// → 2
+
+function countChar(str, checkChar) {
+  let count = 0;
+  for(let i = 0; i <= str.length-1; i++) {
+    (str[i] === checkChar) && (count += 1)
+  }
+  return count;
+}
+console.log(countChar("kakkerlak", "k"));
+// → 4
+
