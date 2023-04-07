a8797d2e308b835c2864bd1adb77f812490882a9
kadir <idia_2008@hotmail.com>
2023-04-08 00:20:15 +0300

    add new file

--- array-methods-exercises/main.js
+++ array-methods-exercises/main.js
@@ -40,3 +40,31 @@ dize elde edilir.
 newString dizesi, fonksiyon tarafından döndürülür.
 Fonksiyonun sonunda, console.log komutu kullanılarak camelize fonksiyonu "This-Word" dizesini 
 alarak çağrılır ve sonuç konsola yazdırılır.*/
+
+//2. Fiter range
+//importance : 4
+// Write a function filterRange(arr, a, b) that gets an array arr, looks for elemnts with values
+// higher or equal to a and lower or equal to b and return a result as an array.
+// The function should not modify the array.It should return the new array.
+// For instance:
+
+let arr = [5, 3, 8, 1];
+
+function filterRange(arr, a, b) {
+  return arr.filter((item) => a <= item && item <= b);
+}
+
+let filtered = filterRange(arr, 1, 4);
+console.log(filtered); //3,1(matching values)
+console.log(arr); //5,3,8,1 (not modified)
+
+//3. Filter range "in place"
+//importance : 4
+//Write a function filterRangeInPlace(arr, a, b) that gets an array and removes from it all values except
+//those that are between a and b.The test is: a <= arr [i] <= b.
+//The function should only modify the array.It should not return anything.
+//For instance:
+
+let arr = [5, 3, 8, 1];
+filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
+alert(arr); // [3,1]
