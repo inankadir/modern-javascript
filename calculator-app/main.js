636162b27c294272982cb5ce67ac1c06605b7745
kadir <idia_2008@hotmail.com>
2023-04-12 03:14:16 +0300

    add new file

--- calculator-app/main.js
+++ calculator-app/main.js
@@ -2,6 +2,9 @@ const display = document.querySelector(".calculator-input");
 const keys = document.querySelector(".calculator-keys");
 
 let displayValue = "0";
+let firstValue = null;
+let operator = null;
+let waitingForSecondValue = false;
 
 updateDisplay();
 
@@ -11,31 +14,71 @@ function updateDisplay() {
 
 keys.addEventListener("click", function (e) {
   const element = e.target;
+  const value = element.value;
 
-  if (element.matches("button")) return;
+  if (!element.matches("button")) return;
 
-  if (element.classList.contains("operator")) {
-    return;
+  switch (value) {
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+    case "=":
+      handleOperator(value);
+      break;
+    case ".":
+      inputDecimal();
+      break;
+    case "clear":
+      clear();
+      break;
+    default:
+      inputNumber(element.value);
   }
-
-  if (element.classList.contains("decimal")) {
-    inputDecimal();
-    updateDisplay();
-    return;
-  }
-
-  if (element.classList.contains("clear")) {
-    clear();
-    updateDisplay();
-    return;
-  }
-
-  inputNumber(element.value);
   updateDisplay();
 });
 
+function handleOperator(nextOperator) {
+  const value = parseFloat(displayValue);
+
+  if (operator && waitingForSecondValue) {
+    operator = nextOperator;
+    return;
+  }
+
+  if (firstValue === null) {
+    firstValue = value;
+  } else if (operator) {
+    const result = calculate(firstValue, value, operator);
+
+    displayValue = `${parseFloat(result.toFixed(7))}`;
+    firstValue = result;
+  }
+
+  waitingForSecondValue = true;
+  operator = nextOperator;
+}
+
+function calculate(first, second, operator) {
+  if (operator === "+") {
+    return first + second;
+  } else if (operator === "-") {
+    return first - second;
+  } else if (operator === "*") {
+    return first * second;
+  } else if (operator === "/") {
+    return first / second;
+  }
+  return second;
+}
+
 function inputNumber(num) {
-  displayValue = displayValue === "0" ? num : displayValue + num;
+  if (waitingForSecondValue) {
+    displayValue = num;
+    waitingForSecondValue = false;
+  } else {
+    displayValue = displayValue === "0" ? num : displayValue + num;
+  }
 }
 
 function inputDecimal() {
