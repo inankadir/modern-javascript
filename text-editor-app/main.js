4399c997457b3e21611b0f6237969abed3629f02
kadir <idia_2008@hotmail.com>
2023-04-16 03:33:12 +0300

    add new file

--- text-editor-app/main.js
+++ text-editor-app/main.js
@@ -1,23 +1,21 @@
 const buttons = document.querySelectorAll('.my-button-text');
-const floatingButton = document.querySelector('floating-button');
+const floatingButton = document.querySelector('.floating-button');
 
-buttons.forEach( button => {
+buttons.forEach(button => {
     button.addEventListener('click', () => {
         let myEvent = button.dataset.command;
-        
-        if (myEvent === 'insertImage' || myEvent === 'createLink'){
+
+        if (myEvent === 'insertImage' || myEvent === 'createLink') {
             let url = prompt('Insert Link Here');
             document.execCommand(myEvent, false, url);
-        }
-        else if (myEvent === 'formatBlock') {
+        } else if (myEvent === 'formatBlock') {
             let formattingValue = button.dataset.block;
             document.execCommand(myEvent, false, formattingValue);
-        }
-        else {
+        } else {
             document.execCommand(myEvent, false, null);
         }
-    })
-})
+    });
+});
 
 floatingButton.addEventListener('click', () => {
     let myHeading = document.querySelector('.title');
@@ -29,13 +27,10 @@ floatingButton.addEventListener('click', () => {
     myHeading.classList.toggle('fullscreen-title');
 
     if (contentOutput.classList.contains('fullscreen-output')) {
-        floatingButton.childNodes[0].classList.remove('fa-expand-arrows-alt')
-        floatingButton.childNodes[0].classList.add('fa-compress-arrows-alt')
+        floatingButton.childNodes[0].classList.remove('fa-expand-arrows-alt');
+        floatingButton.childNodes[0].classList.add('fa-compress-arrows-alt');
+    } else {
+        floatingButton.childNodes[0].classList.add('fa-expand-arrows-alt');
+        floatingButton.childNodes[0].classList.remove('fa-compress-arrows-alt');
     }
-    else {
-        floatingButton.childNodes[0].classList.add('fa-expand-arrows-alt')
-        floatingButton.childNodes[0].classList.remove('fa-compress-arrows-alt')
-
-    }
-
-})
+});
